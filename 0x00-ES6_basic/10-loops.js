1bc208ba6934a5aee242778860bb99bdef0cc14a
amanabiy <amanuelsisay.as@gmail.com>
2021-11-22 23:30:09 +0300

    shorten

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = []
-  for (let word of array) {
-    word = appendString + word;
-    newArray.push(word)
+  const newArray = [];
+  for (const word of array) {
+    newArray.push(appendString + word);
   }
   return newArray;
 }
