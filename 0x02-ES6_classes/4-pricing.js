c16ee66f3a03757ef40fc36b7e8f46d0123028f6
amanabiy <amanuelsisay.as@gmail.com>
2021-11-25 23:01:38 +0300

    correct spelling error

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -28,7 +28,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.name})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
