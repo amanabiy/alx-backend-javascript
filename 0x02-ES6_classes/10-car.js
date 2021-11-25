c7d11905e153f58e899daddc384c7956fc6ade21
amanabiy <amanuelsisay.as@gmail.com>
2021-11-25 22:45:34 +0300

    constructor

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-underscore-dangle */
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
