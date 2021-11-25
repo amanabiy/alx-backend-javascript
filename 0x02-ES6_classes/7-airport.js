f6df88335f12fb64ddd24edf22ca0e7d72c9f8ef
amanabiy <amanuelsisay.as@gmail.com>
2021-11-25 22:35:52 +0300

    hoisting util

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,31 +1,33 @@
-const util = require('util')
+/* eslint-disable no-underscore-dangle */
+const util = require('util');
+
 export default class Airport {
-    constructor (name, code) {
-        this._name = name;
-        this._code = code;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-    set name(name) {
-        this._name = name;
-      }
-    
-      get name() {
-        return this._name;
-      }
-    
-      set code(code) {
-        this._code = code;
-      }
-    
-      get code() {
-        return this._code;
-      }
+  set name(name) {
+    this._name = name;
+  }
 
-    toString() {
-        return `[object ${this.code}]`
-    }
+  get name() {
+    return this._name;
+  }
 
-    [util.inspect.custom]() {
-        return `${this.constructor.name} [${this.code}] { _name: '${this.name}', _code: '${this.code}' }`;
-    }
+  set code(code) {
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+
+  [util.inspect.custom]() {
+    return `${this.constructor.name} [${this.code}] { _name: '${this.name}', _code: '${this.code}' }`;
+  }
 }
