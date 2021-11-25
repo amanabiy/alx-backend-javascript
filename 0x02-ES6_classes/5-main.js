2f29dc4772ca04cf831be2fc44e86a4b7779990c
amanabiy <amanuelsisay.as@gmail.com>
2021-11-25 23:10:24 +0300

    will show error only if extends to other class

--- 0x02-ES6_classes/5-main.js
+++ 0x02-ES6_classes/5-main.js
@@ -1,12 +1,12 @@
+/* eslint-disable no-new */
 import Building from './5-building';
 
 const b = new Building(100);
 console.log(b);
 
 class TestBuilding extends Building {}
-console.log(TestBuilding());
+
 try {
-  // eslint-disable-next-line no-new
   new TestBuilding(200);
 } catch (err) {
   console.log(err);
