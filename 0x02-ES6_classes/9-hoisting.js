b56c0e669f4d6beb99db718cd7f7fd63921b7391
amanabiy <amanuelsisay.as@gmail.com>
2021-11-25 23:18:59 +0300

    use export default

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 
 // eslint-disable-next-line max-classes-per-file
-export class HolbertonClass {
+class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -18,7 +18,7 @@ export class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-export class StudentHolberton {
+class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -44,5 +44,7 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+// export { listOfStudents, HolbertonClass, StudentHolberton };
+export default listOfStudents;
 // console.log(listOfStudents)
