cb68f09f1aa7f6d204a535f69ce2548295898f1f
amanabiy <amanuelsisay.as@gmail.com>
2021-11-30 23:41:07 +0300

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  const { length } = startString;
+  let myString = '';
+  let count = 0;
+  if (!startString || !startString.length) return myString;
+  for (const element of set) {
+    if (element.startsWith(startString)) {
+      if (count === 0) myString = `${element.substring(length)}`;
+      myString = `${myString}-${element.substring(length)}`;
+    }
+    count += 1;
+  }
+  return myString;
+}
