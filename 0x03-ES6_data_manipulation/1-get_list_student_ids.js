6a30a6eb7df893f064e80e27e26d0f148e422197
amanabiy <amanuelsisay.as@gmail.com>
2021-11-30 21:29:03 +0300

    remove unnecessary comments

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,14 +1,7 @@
 export default function getListStudentIds(mylist) {
-//   if (typeof mylist === 'string')
-//     return [];
-//   if (mylist.isArray) {
-//     return [];
-//   }
   if (mylist.constructor.name === 'Array') {
     const newArray = mylist.map((element) => element.id);
     return newArray;
-    // do something
   }
-
   return [];
 }
