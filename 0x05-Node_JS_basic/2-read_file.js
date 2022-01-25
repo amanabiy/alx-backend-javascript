04aedb8105ca0531521d92e6e4a88a2d6bb055d0
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-25 23:58:44 +0300

    read file async

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,5 +1,22 @@
-function countStudents() {
+const fs = require('fs');
 
+function countStudents(path) {
+  try {
+    let data = fs.readFileSync(path, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const studSub = {};
+    for (const row of data) {
+      const student = row.split(',');
+      if (!studSub[student[3]]) studSub[student[3]] = [];
+      studSub[student[3]].push(student[0]);
+    }
+    for (const subject in studSub) {
+      if (subject) console.log(`Number of students in ${subject}: ${studSub[subject].length}. List: ${studSub[subject].join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
-module.exports = countStudents
+module.exports = countStudents;
