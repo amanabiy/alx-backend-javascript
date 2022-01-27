2a5804c1821a6ba91d68d0d3b6525142ca9833aa
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-27 23:40:04 +0300

    added restore for each spy

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -8,11 +8,11 @@ describe('sendPaymentRequestToApi', function() {
     it('should call calculateNumber', function() {
         const calcNumSpy = spy(Utils, 'calculateNumber');
         const consoleSpy = spy(console, 'log');
-        const requestResponse = sendPaymentRequestToApi(1, 2);
+        const requestResponse = sendPaymentRequestToApi(100, 20);
     
-        expect(calcNumSpy.calledOnceWithExactly('SUM', 1, 2)).to.equal(true);
-        expect(consoleSpy.calledWithExactly('The total is: 3')).to.equal(true);
-        expect(Utils.calculateNumber('SUM', 1, 2)).to.equal(requestResponse);
+        expect(calcNumSpy.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
+        expect(consoleSpy.calledWithExactly('The total is: 120')).to.equal(true);
+        expect(Utils.calculateNumber('SUM', 100, 20).to.equal(requestResponse);
         calcNumSpy.restore();
         consoleSpy.restore();
     })
