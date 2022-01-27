c89cb2e8db63db9c753fa65b4aa077db3d2b0bee
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-27 23:27:38 +0300

    test with snin spy

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,16 +5,22 @@ const {expect} = require('chai')
 const spy = sinon.spy
 const sendPaymentRequestToApi = require('./3-payment')
 describe('sendPaymentRequestToApi', function() {
+    afterEach(() => {
+        // Restore the default sandbox here
+        sinon.restore();
+      });
+    beforeEach(() => {
+        // setup sinon
+
+    })
+
     it('should call calculateNumber', function() {
         const calcNumSpy = spy(Utils, 'calculateNumber');
         const consoleSpy = spy(console, 'log');
-        const requestResponse = sendPaymentRequestToApi(100, 20);
+        const requestResponse = sendPaymentRequestToApi(1, 2);
     
-        expect(calcNumSpy.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
-        expect(consoleSpy.calledWithExactly('The total is: 120')).to.equal(true);
-        expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(requestResponse);
-    
-        calcNumSpy.restore();
-        consoleSpy.restore();
+        expect(calcNumSpy.calledOnceWithExactly('SUM', 1, 2)).to.equal(true);
+        expect(consoleSpy.calledWithExactly('The total is: 3')).to.equal(true);
+        expect(Utils.calculateNumber('SUM', 1, 2)).to.equal(requestResponse);
     })
 })
