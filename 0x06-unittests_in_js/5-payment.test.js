67b1b720902eb40e34d4ae09cb8e5d368af8f52e
Amanuel Sisay <amanuelsisay.as@gmail.com>
2022-01-28 00:33:01 +0300

    3-payment

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,19 +1,20 @@
 const Utils = require('./utils');
-const {spy} = require('sinon');
+const {spy, stub} = require('sinon');
 const {expect} = require('chai')
 
 const sendPaymentRequestToApi = require('./3-payment')
 describe('sendPaymentRequestToApi', function() {
 
-    it('should call calculateNumber', function() {
-        const calcNumSpy = spy(Utils, 'calculateNumber');
+    it('should call calculateNumber stubs', function() {
         const consoleSpy = spy(console, 'log');
+        const calcStub = stub(Utils, "calculateNumber").returns(10);
         const requestResponse = sendPaymentRequestToApi(100, 20);
-    
-        expect(calcNumSpy.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
-        expect(consoleSpy.calledWithExactly('The total is: 120')).to.equal(true);
+
+        expect(calcStub.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
         expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(requestResponse);
-        calcNumSpy.restore();
+        expect(consoleSpy.calledWithExactly('The total is: 10')).to.equal(true);
+
+        calcStub.restore();
         consoleSpy.restore();
     })
 })
